99efe213e5c2dabb13f3901da30b1d1cbb8f76c9
PhongRepper <h.phong1002@gmail.com>
2017-11-02 10:18:49 +0700

    initial commit

---
+++ Session 2/homework/rua1.py
@@ -0,0 +1,29 @@
+from turtle import *
+shape("turtle")
+color("blue")
+left(60)
+forward(50)
+right(120)
+forward(50)
+right(120)
+forward(50)
+
+for i in range(4):
+    color("red")
+    right(90)
+    forward(50)
+for i in range(5):
+    color("blue")
+    right(60)
+    forward(50)
+
+right(60)
+forward(50)
+
+for n in range(7):
+    color("red")
+    right(45)
+    forward(50)
+
+
+mainloop()

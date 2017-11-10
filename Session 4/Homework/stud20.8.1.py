76c0765eacb247a67270379507c9a586a5f0aeac
PhongRepper <h.phong1002@gmail.com>
2017-11-10 19:46:05 +0700

    blad

---
+++ Session 4/Homework/stud20.8.1.py
@@ -0,0 +1,9 @@
+sentence = input("Enter a sentence: ")
+sentence= sentence.lower()
+def count_dict(sentence):
+    wo = {}
+    for letter in sentence:
+        wo[letter] = sentence.count(letter)
+    for _ in sorted(wo):
+        print(_,' ', str(wo[_]))
+count_dict(sentence)
